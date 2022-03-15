a8b4537483153327c423b962bd5290e06501ad14
Zachary Johnson <zachtheblu33212@gmail.com>
2022-03-15 01:23:31 -0700

    Minor Changes

---
+++ CS296N_Term_Project_Unit_Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using CS296N_Term_Project.Models;
+using CS296N_Term_Project.Controllers;
+using System;
+using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+using CS296N_Term_Project.Models.DataLayer;
+using Microsoft.AspNetCore.Identity;
+
+namespace CS296N_Term_Project_Unit_Tests
+{
+    public class ImageTests
+    {
+        [Fact]
+        public void PullGroupImage()
+        {
+            
+        }
+    }
+}
